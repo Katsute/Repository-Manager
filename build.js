fb7dc5d3e37f28bd5a45642bb598c61f662da0c5
Katsute <58778985+Katsute@users.noreply.github.com>
2023-02-25 14:41:33 -0500

    add additional svgs

--- build.js
+++ build.js
@@ -4,11 +4,43 @@ const path = require("path");
 const folder = path.join(__dirname, "svg");
 
 const write = (key, color) => {
-    let svg =
-        `<svg width="1" height="1" xmlns="http://www.w3.org/2000/svg">` +
-        `<rect width="1" height="1" fill="${color}"/>` +
-        `</svg>`;
-    fs.writeFileSync(path.join(folder, `${key}.svg`), svg, "utf-8");
+    {
+        const dest = path.join(folder, "static");
+        fs.existsSync(dest) || fs.mkdirSync(dest);
+
+        let svg =
+            `<svg width="1" height="1" xmlns="http://www.w3.org/2000/svg">` +
+            `<rect width="1" height="1" fill="${color}"/>` +
+            `</svg>`;
+
+        fs.writeFileSync(path.join(dest, `${key}.svg`), svg, "utf-8");
+    }
+    {
+        const dest = path.join(folder, "animated");
+        fs.existsSync(dest) || fs.mkdirSync(dest);
+
+        let svg =
+            `<svg width="1" height="1" xmlns="http://www.w3.org/2000/svg">` +
+            `<rect width="1" height="1" fill="${color}">` +
+            `<animate attributeType="XML" attributeName="fill" values="${color};transparent;${color}" dur="1s" repeatCount="indefinite"/>` +
+            `</rect>` +
+            `</svg>`;
+
+        fs.writeFileSync(path.join(dest, `${key}.svg`), svg, "utf-8");
+    }
+    {
+        const dest = path.join(folder, "blink");
+        fs.existsSync(dest) || fs.mkdirSync(dest);
+
+        let svg =
+            `<svg width="1" height="1" xmlns="http://www.w3.org/2000/svg">` +
+            `<rect width="1" height="1" fill="${color}">` +
+            `<animate attributeType="XML" attributeName="fill" values="${color};transparent" dur="0.75s" calcMode="discrete" repeatCount="indefinite"/>` +
+            `</rect>` +
+            `</svg>`;
+
+        fs.writeFileSync(path.join(dest, `${key}.svg`), svg, "utf-8");
+    }
 };
 
 !fs.existsSync(folder) || fs.rmSync(folder, {recursive: true});
